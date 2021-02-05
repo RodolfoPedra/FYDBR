44426f593e8938803c89e7a7c4206dac148f504f
RodolfoPedra <rodolfoaldrovandip@gmail.com>
2021-02-04 22:03:41 -0400

    ajustes alinhamentos art is the weapon

--- packages/mars-theme/src/components/art-weapon.js
+++ packages/mars-theme/src/components/art-weapon.js
@@ -3,26 +3,77 @@ import { connect, styled } from "frontity";
 import { Container } from "../assets/css-in-js/GlobalStyles";
 
 const InfoWeapon = () => {
-  const [wAvatar, setWAvatar] = React.useState(null);
-
-  React.useEffect(() => {
-    setWAvatar(document.querySelector("#avatar").getBoundingClientRect().width);
-  }, [wAvatar]);
 
   return (
     <>
       <ContainerInfoWeapon>
-        <Avatar id="avatar" hAvatar={wAvatar} />
+        <Avatar />
         <ContainerInfos>
           <h2>Fydbr </h2>
-          <p>Especial danger days:</p>
-          <span>
+          <h3>Especial danger days:</h3>
+          <p>
             fique por dentro do quadrinho “Killjoys” em nossa resenha exclusiva
-          </span>
-          <p>Especial danger days:</p>
-          <span>
+          </p>
+          <h3>Especial danger days:</h3>
+          <p>
             fique por dentro do quadrinho “Killjoys” em nossa resenha exclusiva
-          </span>
+          </p>
+        </ContainerInfos>
+      </ContainerInfoWeapon>
+      <ContainerInfoWeapon>
+        <Avatar />
+        <ContainerInfos>
+          <h2>Fydbr </h2>
+          <h3>Especial danger days:</h3>
+          <p>
+            fique por dentro do quadrinho “Killjoys” em nossa resenha exclusiva
+          </p>
+          <h3>Especial danger days:</h3>
+          <p>
+            fique por dentro do quadrinho “Killjoys” em nossa resenha exclusiva
+          </p>
+        </ContainerInfos>
+      </ContainerInfoWeapon>
+      <ContainerInfoWeapon>
+        <Avatar />
+        <ContainerInfos>
+          <h2>Fydbr </h2>
+          <h3>Especial danger days:</h3>
+          <p>
+            fique por dentro do quadrinho “Killjoys” em nossa resenha exclusiva
+          </p>
+          <h3>Especial danger days:</h3>
+          <p>
+            fique por dentro do quadrinho “Killjoys” em nossa resenha exclusiva
+          </p>
+        </ContainerInfos>
+      </ContainerInfoWeapon>
+      <ContainerInfoWeapon>
+        <Avatar />
+        <ContainerInfos>
+          <h2>Fydbr </h2>
+          <h3>Especial danger days:</h3>
+          <p>
+            fique por dentro do quadrinho “Killjoys” em nossa resenha exclusiva
+          </p>
+          <h3>Especial danger days:</h3>
+          <p>
+            fique por dentro do quadrinho “Killjoys” em nossa resenha exclusiva
+          </p>
+        </ContainerInfos>
+      </ContainerInfoWeapon>
+      <ContainerInfoWeapon>
+        <Avatar />
+        <ContainerInfos>
+          <h2>Fydbr </h2>
+          <h3>Especial danger days:</h3>
+          <p>
+            fique por dentro do quadrinho “Killjoys” em nossa resenha exclusiva
+          </p>
+          <h3>Especial danger days:</h3>
+          <p>
+            fique por dentro do quadrinho “Killjoys” em nossa resenha exclusiva
+          </p>
         </ContainerInfos>
       </ContainerInfoWeapon>
     </>
@@ -30,13 +81,24 @@ const InfoWeapon = () => {
 };
 
 const ArtWeapon = ({ state, libraries }) => {
+  const author = 45068819;
+
+  const resStateMod = state.source.get({
+    url: "https://fakeyourdeathbr.com",
+    params: {
+      author
+    },
+  });
+
+  console.log('res author mod: ', resStateMod);
+
   React.useEffect(async () => {
-    const author = 45068819;
+    // , 182061584, 182066183, 182066187];
     const { api, populate, getTotalPages } = libraries.source;
     const response = await api.get({
       endpoint: "posts",
       params: {
-        author,
+        author
       },
     });
     const firstItems = await populate({
@@ -66,7 +128,7 @@ export default connect(ArtWeapon);
 
 const ExtContainer = styled(Container)`
   width: 100%;
-  height: 500px;
+  /* height: 500px; */
   background: #000;
   padding: 0;
   display: flex;
@@ -79,8 +141,9 @@ const StyledContainer = styled(Container)`
   color: #fff;
   display: flex;
   flex-wrap: wrap;
-  flex-direction: column;
-  align-items: flex-start;
+  justify-content: flex-start;
+  /* flex-direction: column;
+  align-items: flex-start; */
 `;
 
 const TittleArtWeapon = styled.h1`
@@ -92,36 +155,63 @@ const TittleArtWeapon = styled.h1`
 `;
 
 const ContainerInfoWeapon = styled.div`
+	flex-grow: 1;
+  width: 33%;
+  max-width: 33%;
+	height: 240px;
   display: flex;
-  padding-top: 50px;
-  max-width: 25vw;
+  &:nth-of-type(-n+3) {
+    padding-top: 60px;
+    margin-bottom: 16px;
+  }
+
+  @media screen and (max-width: 1550px) {
+   &:nth-of-type(-n+3) {
+    margin-bottom: 60px;
+  }
+  }
+
+  @media screen and (max-width: 1020px) {
+   &:nth-of-type(-n+3) {
+    width: 50%;
+    max-width: 50%;
+    margin-bottom: 100px;
+  }
+  }
 `;
 
 const ContainerInfos = styled.div`
+  width: 23vw;
+  padding-left: 14px;
   font-size: 21px;
   h2 {
     font-family: DINProCondBold;
     color: #c10000;
     text-transform: uppercase;
+    padding-bottom: 4px;
   }
 
-  p {
+  h3 {
     font-family: DINProCondBold;
     text-transform: uppercase;
   }
 
-  span {
-    font-family: DINProCondMedium;
-  }
+  p {
+    font-family: DINProLight;
+    padding-top: 4px;
+    padding-bottom: 12px;
+   }
 `;
 
 const Avatar = styled.img`
+  @media screen and (min-width: 1900px) {
+    width: 72px;
+    height: 72px;
+  }
   width: 3.75vw;
-  height: 42px;
+  height: 3.75vw;
   min-width: 46px;
   min-height: 46px;
-  max-width: 72px;
-  max-height: 72px;
   background: #fff;
   border-radius: 50px;
 `;
